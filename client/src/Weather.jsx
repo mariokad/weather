670b9d2727096437bff8b484adb0d0673661b697
Marissa <marissaokada@gmail.com>
2017-02-04 16:05:34 -0800

    Add basic dropdown menu

--- client/src/Weather.jsx
+++ client/src/Weather.jsx
@@ -3,9 +3,26 @@ import React from 'react';
 export default class Weather extends React.Component {
   constructor() {
     super();
+    this.state = {
+      area: 'San Francisco, CA'
+    }
   }
-  
+
+  handleSelect(e) {
+    this.setState({area: e.target.value});
+  }
+
   render() {
-    return <h1>hi</h1>
+    var message = 'You selected ' + this.state.area;
+    return (
+      <div>
+        <select defaultValue={this.state.area} onChange={this.handleSelect.bind(this)}>
+          <option value="San Francisco, CA">San Francisco, CA</option>
+          <option value="New York, NY">New York, NY</option>
+          <option value="Seattle, WA">Seattle, WA</option>
+        </select>
+        <p>{message}</p>
+      </div>
+    );
   }
 }
